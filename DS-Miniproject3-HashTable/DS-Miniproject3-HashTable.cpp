0b38aef8c44c0e0163f0c90b88cf338f441a5516
280051hubert <280051@student.pwr.edu.pl>
2025-06-10 20:30:55 +0200

    Major DS-Miniproject3-HashTable.cpp rewrite
    Wrong measurering method after I analysed the code

--- DS-Miniproject3-HashTable/DS-Miniproject3-HashTable.cpp
+++ DS-Miniproject3-HashTable/DS-Miniproject3-HashTable.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <random>
 #include <unordered_set>
+#include <memory>
 
 #include "HashTable_OpenAddressing.hpp"
 #include "HashTable_Chain.hpp"
@@ -13,7 +14,7 @@ using namespace std::chrono;
 
 const int NUM_TESTS = 50;
 const vector<int> TEST_SIZES = { 5000, 8000, 10000, 16000, 20000, 40000, 60000, 100000 };
-
+const vector<double> FILL_PERCENTAGES = { 0.2, 0.5, 0.9 };
 struct TestData {
     vector<int> keys;
     vector<int> values;
@@ -22,86 +23,124 @@ struct TestData {
 
 TestData generateTestData(int size) {
     TestData data;
-    unordered_set<int> used;
+    unordered_set<int> used_keys;
     random_device rd;
     mt19937 gen(rd());
-    uniform_int_distribution<> dis(1, size * 10);
+
+    uniform_int_distribution<> key_dis(1, size * 20);
+    uniform_int_distribution<> val_dis(1, size * 10);
 
     while ((int)data.keys.size() < size) {
-        int key = dis(gen);
-        if (used.insert(key).second) {
+        int key = key_dis(gen);
+        if (used_keys.insert(key).second) {
             data.keys.push_back(key);
-            data.values.push_back(dis(gen));
+            data.values.push_back(val_dis(gen));
         }
     }
     return data;
 }
 
-template <typename Func>
-long long measureAverageTime(Func f, int iterations) {
-    long long total = 0;
+
+template <typename TableType, typename Func>
+long long measureAverageTime(int iterations, int table_size, const TestData& initial_data, int num_elements_to_prefill, int num_elements_to_operate, Func f_op) {
+    long long total_time = 0;
+
     for (int i = 0; i < iterations; ++i) {
+        //nowe tablice dla iteracji
+        unique_ptr<TableType> table = make_unique<TableType>(table_size);
+
+        //wypelnianie dla danego stanu
+        for (int j = 0; j < num_elements_to_prefill; ++j) {
+            table->insert(initial_data.keys[j], initial_data.values[j]);
+        }
+
         auto start = high_resolution_clock::now();
-        f();
+        //lambda decyduje jakie lementy insert remove
+        f_op(*table, initial_data, num_elements_to_prefill, num_elements_to_operate);
         auto end = high_resolution_clock::now();
-        total += duration_cast<nanoseconds>(end - start).count();
+        total_time += duration_cast<nanoseconds>(end - start).count();
     }
-    return total / iterations;
+    return total_time / iterations;
 }
 
 int main() {
     for (int size : TEST_SIZES) {
-        TestData data = generateTestData(size);
+        TestData full_data = generateTestData(static_cast<int>(size * 1.1)); 
 
-        {
-            HashOpenAddressing table(size); 
-            auto insertTime = measureAverageTime([&]() {
-                for (int i = 0; i < size; ++i)
-                    table.insert(data.keys[i], data.values[i]);
-                }, NUM_TESTS);
+        for (double fill_percent : FILL_PERCENTAGES) {
+            int num_elements_current_fill = static_cast<int>(size * fill_percent);
 
-            cout << "OpenAddressing;" << size << ";insert;" << insertTime << "\n";
+            if (num_elements_current_fill == 0 && fill_percent > 0) {
+                num_elements_current_fill = 1;
+            }
+            if (num_elements_current_fill > size) {
+                num_elements_current_fill = size;
+            }
 
-            auto removeTime = measureAverageTime([&]() {
-                for (int i = 0; i < size; ++i)
-                    table.remove(data.keys[i]);
-                }, NUM_TESTS);
+            int num_elements_to_insert_batch = max(1, static_cast<int>(size * 0.01));
+            if (num_elements_current_fill + num_elements_to_insert_batch > size) {
+                num_elements_to_insert_batch = size - num_elements_current_fill;
+            }
+            if (num_elements_to_insert_batch <= 0) { 
+                num_elements_to_insert_batch = 0;
+            }
 
-            cout << "OpenAddressing;" << size << ";remove;" << removeTime << "\n";
-        }
+            {
+                if (num_elements_to_insert_batch > 0) { 
+                    auto insertTime = measureAverageTime<HashOpenAddressing>(NUM_TESTS, size, full_data, num_elements_current_fill, num_elements_to_insert_batch,
+                        [&](HashOpenAddressing& table, const TestData& data_source, int prefill_count, int insert_count) {
+                            for (int i = 0; i < insert_count; ++i) {
+                                table.insert(data_source.keys[prefill_count + i], data_source.values[prefill_count + i]);
+                            }
+                        });
+                    cout << "OpenAddressing;" << size << ";" << (fill_percent * 100) << "%;insert;" << insertTime << "\n";
+                }
+                auto removeTime = measureAverageTime<HashOpenAddressing>(NUM_TESTS, size, full_data, num_elements_current_fill, num_elements_current_fill,
+                    [&](HashOpenAddressing& table, const TestData& data_source, int prefill_count, int remove_count) {
+                        for (int i = 0; i < remove_count; ++i) {
+                            table.remove(data_source.keys[i]);
+                        }
+                    });
+                cout << "OpenAddressing;" << size << ";" << (fill_percent * 100) << "%;remove;" << removeTime << "\n";
+            }
+            {
+                if (num_elements_to_insert_batch > 0) {
+                    auto insertTime = measureAverageTime<HashChain>(NUM_TESTS, size, full_data, num_elements_current_fill, num_elements_to_insert_batch,
+                        [&](HashChain& table, const TestData& data_source, int prefill_count, int insert_count) {
+                            for (int i = 0; i < insert_count; ++i) {
+                                table.insert(data_source.keys[prefill_count + i], data_source.values[prefill_count + i]);
+                            }
+                        });
+                    cout << "Chaining;" << size << ";" << (fill_percent * 100) << "%;insert;" << insertTime << "\n";
+                }
+                auto removeTime = measureAverageTime<HashChain>(NUM_TESTS, size, full_data, num_elements_current_fill, num_elements_current_fill,
+                    [&](HashChain& table, const TestData& data_source, int prefill_count, int remove_count) {
+                        for (int i = 0; i < remove_count; ++i) {
+                            table.remove(data_source.keys[i]);
+                        }
+                    });
+                cout << "Chaining;" << size << ";" << (fill_percent * 100) << "%;remove;" << removeTime << "\n";
+            }
 
-        {
-            HashChain table(size);
-            auto insertTime = measureAverageTime([&]() {
-                for (int i = 0; i < size; ++i)
-                    table.insert(data.keys[i], data.values[i]);
-                }, NUM_TESTS);
-
-            cout << "Chaining;" << size << ";insert;" << insertTime << "\n";
-
-            auto removeTime = measureAverageTime([&]() {
-                for (int i = 0; i < size; ++i)
-                    table.remove(data.keys[i]);
-                }, NUM_TESTS);
-
-            cout << "Chaining;" << size << ";remove;" << removeTime << "\n";
-        }
-
-        {
-            HashTable_AVL table(size);
-            auto insertTime = measureAverageTime([&]() {
-                for (int i = 0; i < size; ++i)
-                    table.insert(data.keys[i], data.values[i]);
-                }, NUM_TESTS);
-
-            cout << "AVL;" << size << ";insert;" << insertTime << "\n";
-
-            auto removeTime = measureAverageTime([&]() {
-                for (int i = 0; i < size; ++i)
-                    table.remove(data.keys[i]);
-                }, NUM_TESTS);
-
-            cout << "AVL;" << size << ";remove;" << removeTime << "\n";
+            
+            {
+                if (num_elements_to_insert_batch > 0) {
+                    auto insertTime = measureAverageTime<HashTable_AVL>(NUM_TESTS, size, full_data, num_elements_current_fill, num_elements_to_insert_batch,
+                        [&](HashTable_AVL& table, const TestData& data_source, int prefill_count, int insert_count) {
+                            for (int i = 0; i < insert_count; ++i) {
+                                table.insert(data_source.keys[prefill_count + i], data_source.values[prefill_count + i]);
+                            }
+                        });
+                    cout << "AVL;" << size << ";" << (fill_percent * 100) << "%;insert;" << insertTime << "\n";
+                }
+                auto removeTime = measureAverageTime<HashTable_AVL>(NUM_TESTS, size, full_data, num_elements_current_fill, num_elements_current_fill,
+                    [&](HashTable_AVL& table, const TestData& data_source, int prefill_count, int remove_count) {
+                        for (int i = 0; i < remove_count; ++i) {
+                            table.remove(data_source.keys[i]);
+                        }
+                    });
+                cout << "AVL;" << size << ";" << (fill_percent * 100) << "%;remove;" << removeTime << "\n";
+            }
         }
     }
 
