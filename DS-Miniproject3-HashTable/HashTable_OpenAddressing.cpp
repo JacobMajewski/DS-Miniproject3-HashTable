4c5d367c4dd87304dc77e09fd30771b700b06ff3
280051hubert <280051@student.pwr.edu.pl>
2025-06-10 17:11:31 +0200

    HashTableOpenAddressing.cpp size fix

--- DS-Miniproject3-HashTable/HashTable_OpenAddressing.cpp
+++ DS-Miniproject3-HashTable/HashTable_OpenAddressing.cpp
@@ -1,27 +1,33 @@
 #include "HashTable_OpenAddressing.hpp"
 
-HashOpenAddressing::HashOpenAddressing()
+HashOpenAddressing::HashOpenAddressing(int size) : tableSize(size)
 {
-    for (int i = 0; i < TABLE_SIZE; ++i)
+    table = new Entry[tableSize];
+    for (int i = 0; i < tableSize; ++i)
     {
         table[i].isOccupied = false;
         table[i].isDeleted = false;
     }
 }
 
+HashOpenAddressing::~HashOpenAddressing()
+{
+    delete[] table;
+}
+
 int HashOpenAddressing::hash(int key)
 {
-    return key % TABLE_SIZE;
+    return key % tableSize;
 }
 
 int HashOpenAddressing::probe(int key, int i)
 {
-    return (hash(key) + i) % TABLE_SIZE;
+    return (hash(key) + i) % tableSize;
 }
 
 void HashOpenAddressing::insert(int key, int elem)
 {
-    for (int i = 0; i < TABLE_SIZE; ++i)
+    for (int i = 0; i < tableSize; ++i)
     {
         int idx = probe(key, i);
         if (!table[idx].isOccupied || table[idx].isDeleted)
@@ -37,7 +43,7 @@ void HashOpenAddressing::insert(int key, int elem)
 
 int HashOpenAddressing::remove(int key)
 {
-    for (int i = 0; i < TABLE_SIZE; ++i)
+    for (int i = 0; i < tableSize; ++i)
     {
         int idx = probe(key, i);
         if (table[idx].isOccupied && table[idx].key == key)
